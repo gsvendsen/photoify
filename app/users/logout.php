ced67a03b4dfdc035497e006c86eb1140d0473e6
Gustav Svendsen <gsvendsen99@gmail.com>
2018-12-12 10:04:36 +0100

    Added user logout logic

---
+++ app/users/logout.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+// In this file we logout users.
+unset($_SESSION['user']);
+
+$_SESSION['messages'][] = "Logged out successfully!";
+
+
+redirect("/");
