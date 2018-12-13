dc3c977f16576aaf93ab6bdfee571089e1b7c2ee
Gustav Svendsen <gsvendsen99@gmail.com>
2018-12-13 11:45:43 +0100

    Updated navigation to dropdown menu

--- views/navigation.php
+++ views/navigation.php
@@ -2,27 +2,22 @@
   <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
 
   <ul class="navbar-nav">
-      <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['REQUEST_URI'] == "/index.php" || $_SERVER['REQUEST_URI'] == "/"){echo "active";} ?>" href="/index.php">Home</a>
-      </li><!-- /nav-item -->
       <!-- If logged out -->
       <?php if (!isset($_SESSION['user'])): ?>
-        <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['REQUEST_URI'] == "/login.php"){echo "active";} ?>" href="/login.php">Log In</a>
-        </li><!-- /nav-item -->
 
-        <li class="nav-item">
-          <a class="nav-link <?php if($_SERVER['REQUEST_URI'] == "/register.php"){echo "active";} ?>" href="/register.php">Create Account</a>
-        </li><!-- /nav-item -->
       <!-- If logged in -->
       <?php else: ?>
-        <li class="nav-item">
-          <a class="nav-link" href="/profile.php">My Profile</a>
-        </li><!-- /nav-item -->
+          <div class="dropdown show">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Home
+            </a>
 
-        <li class="nav-item">
-          <a class="nav-link" href="/app/users/logout.php">Log Out</a>
-        </li><!-- /nav-item -->
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="/">Home</a>
+              <a class="dropdown-item" href="/profile.php">My Profile</a>
+              <a class="dropdown-item" href="/app/users/logout.php">Log Out</a>
+            </div>
+          </div>
       <?php endif; ?>
   </ul><!-- /navbar-nav -->
 </nav><!-- /navbar -->
