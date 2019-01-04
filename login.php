34ab4c7905ec2e16b2c4bb5097444c09c3dde763
Gustav Svendsen <gsvendsen99@gmail.com>
2019-01-04 12:59:58 +0100

    Updated classes to custom styling

--- login.php
+++ login.php
@@ -6,20 +6,25 @@
       </div>
     <?php endif;?>
 
+    <h2>Log In</h2>
+
     <form action="app/users/login.php" method="post">
-        <div class="form-group">
+        <div class="form-section">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" placeholder="example@mail.com" value="<?php if(isset($_SESSION['error'])){ echo $_SESSION['error']['email'];} ?>" required>
-            <small class="form-text text-muted">Please provide the your email address.</small>
+            <input type="email" name="email" placeholder="example@mail.com" value="<?php if(isset($_SESSION['error'])){ echo $_SESSION['error']['email'];} ?>" required>
+            <small>Please provide the your email address.</small>
         </div><!-- /form-group -->
 
-        <div class="form-group">
+        <div class="form-section">
             <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+            <input type="password" name="password" required>
+            <small>Please provide the your password (passphrase).</small>
         </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Sign In</button>
+        <div class="form-center">
+            <button type="submit">Sign In</button>
+            <p>Don't have an account? <a href="?q=register">Register</a>
+        </div>
     </form>
 </article>
 <?php if(isset($_SESSION['error'])){ unset($_SESSION['error']) ;}; ?>
