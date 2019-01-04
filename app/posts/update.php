092e27cb274ea9cb61d427132a825c94747ca6c4
Gustav Svendsen <gsvendsen99@gmail.com>
2019-01-04 13:00:45 +0100

    Added logic for editing post

---
+++ app/posts/update.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+
+if(!isset($_SESSION['user'], $_POST['description'], $_GET['post'])){
+    redirect('/');
+} else {
+
+    $newDescription = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
+    $postId = filter_var($_GET['post'], FILTER_SANITIZE_STRING);
+
+    $updateStatement = $pdo->prepare("UPDATE posts SET description = :description WHERE id = :post_id");
+
+    $updateStatement->bindParam(':description', $newDescription, PDO::PARAM_STR);
+    $updateStatement->bindParam(':post_id', $postId, PDO::PARAM_STR);
+
+    $updateStatement->execute();
+
+    $_SESSION['messages'][] = "Your post has been updated!";
+
+}
+
+redirect("/");
