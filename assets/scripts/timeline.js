03d13553cfae6d7c088366454c0e746688f336a6
Gustav Svendsen <gsvendsen99@gmail.com>
2019-01-07 14:47:39 +0100

    Create file which creates user timeline which shows following posts

---
+++ assets/scripts/timeline.js
@@ -0,0 +1,38 @@
+const postContainer = document.querySelector('.post-wrapper')
+
+function Comparator(a, b) {
+  if (a[1] < b[1]) return -1;
+  if (a[1] > b[1]) return 1;
+  return 0;
+}
+
+const createTimeline = (timelineData) => {
+    console.log(timelineData)
+    postContainer.innerHTML += createPosts2(timelineData);
+}
+
+
+
+function fetchTimeline(userId) {
+  fetch('../../app/api/timeline.php?user='+userId)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    createTimeline(myJson);
+  });
+}
+
+function fetchUserId() {
+  fetch('../../app/api/user.php')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    fetchTimeline(myJson.id)
+  });
+}
+
+if(getUrlParameter('u') == ""){
+    fetchUserId()
+}
