3780ebb1382d0ac595d2373427000dc438ac41c6
Gustav Svendsen <gsvendsen99@gmail.com>
2019-01-14 13:45:10 +0100

    Added form class

--- post.php
+++ post.php
@@ -2,8 +2,10 @@
 
 
 <?php if(!isset($_SESSION['user'])){ redirect("/"); } else { $user = $_SESSION['user'];}?>
-<article>
+<div class="new-post-container">
     <h1>New Post</h1>
+
+    <img class="post-image" id='output'>
 
     <?php if(isset($_SESSION['error'])): ?>
       <div class="alert alert-danger" role="alert">
@@ -14,17 +16,17 @@
     <form action="app/posts/create.php" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <label for="image">Image</label>
-            <input class="form-control" type="file" name="image">
+            <input onchange='openFile(event)' class="form-control" type="file" name="image">
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="description">Description</label>
-            <textarea class="form-control" type="text" name="description"></textarea>
+            <textarea placeholder="Image description..." class="form-control" type="text" name="description"></textarea>
         </div><!-- /form-group -->
 
         <button type="submit" class="btn btn-primary">Upload new Post</button>
     </form>
-</article>
+</div>
 <?php if(isset($_SESSION['error'])){ unset($_SESSION['error']) ;}; ?>
 
 <?php require __DIR__.'/views/footer.php'; ?>
