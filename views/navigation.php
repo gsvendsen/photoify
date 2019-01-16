f19215fb595fea71c9aea90fc39ab19147b5d2dc
Gustav Svendsen <gsvendsen99@gmail.com>
2019-01-16 23:22:01 +0100

    Add hidden desktop class for bottom nav bar

--- views/navigation.php
+++ views/navigation.php
@@ -7,14 +7,14 @@ declare(strict_types=1);
 <?php if(isset($_SESSION['user'])): ?>
 	<div class="top-bar">
         <a href="/?u=<?= $_SESSION['user']['username']?>"><img class="post-profile top-profile" src="<?= $_SESSION['user']['img_path'] ?>" alt=""></a>
-        <a href="/">Home</a>
+        <a href="/"><img class="nav-logo" src="assets/images/photoify.png"></a>
         <div class="hamburger-menu">
 			<div class="bar"></div>
 		</div><!-- /hamburger-menu -->
 	</div><!-- /navbar -->
 
     <!-- Navbar for mobile/tablet -->
-	<div class="bottom-bar">
+	<div class="bottom-bar mobile">
         <a href="/post.php">
             <svg class="bar-icon post-bar-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 275.836 275.836" style="enable-background:new 0 0 275.836 275.836;" xml:space="preserve">
             	<path fill="white" d="M191.344,20.922l-95.155,95.155c-0.756,0.756-1.297,1.699-1.565,2.734l-8.167,31.454c-0.534,2.059,0.061,4.246,1.565,5.751   c1.14,1.139,2.671,1.757,4.242,1.757c0.503,0,1.009-0.063,1.508-0.192l31.454-8.168c1.035-0.269,1.979-0.81,2.734-1.565   l95.153-95.153c0.002-0.002,0.004-0.003,0.005-0.004s0.003-0.004,0.004-0.005l19.156-19.156c2.344-2.343,2.344-6.142,0.001-8.484   L218.994,1.758C217.868,0.632,216.343,0,214.751,0c-1.591,0-3.117,0.632-4.242,1.758l-19.155,19.155   c-0.002,0.002-0.004,0.003-0.005,0.004S191.346,20.921,191.344,20.922z M120.631,138.208l-19.993,5.192l5.191-19.993l89.762-89.762   l14.801,14.802L120.631,138.208z M214.751,14.485l14.801,14.802l-10.675,10.675L204.076,25.16L214.751,14.485z"/>
