f56aed8e7d0865f7171ed48ae7d04bb5834fe4bd
Gustav Svendsen <gsvendsen99@gmail.com>
2019-01-16 22:38:38 +0100

    Moved login.php and register.php to views

--- views/intro.php
+++ views/intro.php
@@ -1,11 +1,11 @@
 <?php if(isset($_GET['q'])): ?>
     <?php if($_GET['q'] == 'register'): ?>
-        <?php require __DIR__.'/../register.php'; ?>
+        <?php require __DIR__.'/register.php'; ?>
     <?php elseif($_GET['q'] == 'login'): ?>
-        <?php require __DIR__.'/../login.php'; ?>
+        <?php require __DIR__.'/login.php'; ?>
     <?php else: ?>
-        <?php require __DIR__.'/../login.php'; ?>
+        <?php require __DIR__.'/login.php'; ?>
     <?php endif; ?>
 <?php else: ?>
-    <?php require __DIR__.'/../login.php'; ?>
+    <?php require __DIR__.'/login.php'; ?>
 <?php endif; ?>
